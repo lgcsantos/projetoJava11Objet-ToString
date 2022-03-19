0169fa7c9a10fa9a1effd832bacccb755b370ded
Lucas Santos <lucas.prog@outlook.com>
2022-03-19 12:04:21 -0300

    Commit Inclusão_CarrinhoDeCompras

--- src/br/com/comandosimples/Main.java
+++ src/br/com/comandosimples/Main.java
@@ -10,10 +10,12 @@ public class Main {
         public static void main(String[] args) {
         CarrinhoDeCompras carrinhoDeCompras = new CarrinhoDeCompras();
 
+        carrinhoDeCompras.adiciona(new Mesa("Verde", 140));
         carrinhoDeCompras.adiciona(new Livro("Banco de Dados", "Lucas Santos", 4524));
         carrinhoDeCompras.adiciona(new Livro("Refatoração", "Martin Flowler", 65));
         carrinhoDeCompras.adiciona(new Mesa("Branca", 250));
         carrinhoDeCompras.adiciona(new Mesa("Verde", 120));
+        carrinhoDeCompras.adiciona(new Mesa("Vermelho", 530));
 
         System.out.println( " Valor Total dos Produtos: " + carrinhoDeCompras.getTotal());
     }
